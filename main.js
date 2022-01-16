ee0c53dd0c412508300353e75b23dcdfe1bead0c
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-01-16 02:46:31 -0600

    successgit add . added a randomizer to tell you what the site recommends randomly each visit git push origin master '

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ function renderCoffee(coffee) {
     let html = '<div class="coffee col-sm-6 align-items-center row coffee-card mb-2 p-1 " id="' + coffee.id + '">';
     html += '<img  id="img' + coffee.id + '" src="http://placeholder.pics/svg/250x250" alt="#" class="img-thumbnail m-auto">';       /*will create a function to find this and customize the src remotely*/
     html += '<div class="w-100"></div>';
-    html += '<div class="d-flex d-nowrap mx-auto"><h5 class="col p-0 m-0">' + coffee.name + '</h5>';
+    html += '<h5 class="text-nowrap mx-auto d-none">This visit we recommend:</h5>';
+    html += '<div class="textBox d-flex d-nowrap mx-auto"><h5 class="col p-0 m-0">' + coffee.name + '</h5>';
     html += '<p class=" col-auto p-1 m-0" style="color: darkgrey">' + coffee.roast + '</p></div>';
     html += '</div>';
     return html;
@@ -65,9 +66,9 @@ function addCoffee(e) {
     e.preventDefault();
     let newRoast = document.querySelector('#createRoast');
     let newName = document.querySelector('#createName');
-    let newId = coffees.length;
-    let newCoffee= {name: newName.value,id: newId,roast: newRoast.value};
-    coffees.push(newCoffee);
+    let newId = filteredCoffees.length +1; /*actually needed a +1 cause list starts with array#0 & id#1.*/
+    let newCoffee= {id: newId, name: newName.value, roast: newRoast.value};
+    filteredCoffees.push(newCoffee);
     updateCoffees();
 }
 
@@ -99,6 +100,22 @@ function Search() {
             }
         }
     }
+    /*I want a new function to randomly select a coffee div and make it shiny, proclaiming it's the recommended coffee for this visit!*/
+function recommend() {
+    let cofy, div, text, i;
+    cofy = Math.floor(Math.random() * 13);  /*gets a random id# for coffee, note: id#14 is at array #13*/
+
+    div = document.getElementById(filteredCoffees[cofy].id);  /*selects that random coffee div by id*/
+    div.classList.add("recommend");                          /*adds the 'recommend' class. will add into css to make do shiny thing*/
+    text = div.getElementsByClassName("d-none");     /*I gave all divs a recommended text, I couldn't add it in between divs otherwise.*/
+    text[0].classList.remove('d-none');                  /*so this removes its display none to reveal the specific coffee's text.*/
+}
+
+recommend();
+
+
+/*I also want to create a function that will create a special div to showcase 3 random coffees per reload!*/
+
 /*
 function image(){
     let i, id;
